d8422e3bd61750b8559a08692c68c917f9544ae6
Internet <Internet@PC243>
2014-01-27 19:35:53 -0600

    actualizacion 1

---
+++ christian/src/christian/Usuario.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package christian;
+
+/**
+ *
+ * @author Internet
+ */
+public class Usuario {
+
+   
+    private float edad;
+    
+    public void setEdad(float edad){
+        if(edad<18)
+        this.edad=edad;
+        
+    }
+    
+}
